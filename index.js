8f92d36dc0663f2620cab61dd44040301fb75acd
Zachary Belford <belfordz66@gmail.com>
2019-02-16 15:15:48 -0800

    feat: add index to export schemas

---
+++ index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  apiWithExamples: require('./service-descriptions/api-with-examples.json'),
+  links: require('./service-descriptions/api-with-examples.json'),
+  petstoreExtended: require('./service-descriptions/api-with-examples.json'),
+  petstore: require('./service-descriptions/api-with-examples.json')
+};
