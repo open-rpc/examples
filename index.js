26f8639ea770b13f74a7c085d568c73c6d856dd8
Shane <jonas.shane@gmail.com>
2019-02-16 21:10:40 -0800

    fix: change key to expanded

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = {
   apiWithExamples: require('./service-descriptions/api-with-examples.json'),
   links: require('./service-descriptions/link-example.json'),
-  petstoreExtended: require('./service-descriptions/petstore-expanded.json'),
+  petstoreExpanded: require('./service-descriptions/petstore-expanded.json'),
   petstore: require('./service-descriptions/petstore.json')
 };
