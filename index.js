07e2c419db149fa884d1d27beea6ba6d2364a641
Zachary Belford <belfordz66@gmail.com>
2019-03-04 13:57:52 -0800

    fix: update with by-name stuff

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ module.exports = {
   links: require('./service-descriptions/link-example-openrpc.json'),
   petstore: require('./service-descriptions/petstore-openrpc.json'),
   petstoreExpanded: require('./service-descriptions/petstore-expanded-openrpc.json'),
-  simpleMath: require('./service-descriptions/simple-math-openrpc.json')
+  simpleMath: require('./service-descriptions/simple-math-openrpc.json'),
+  petstore: require('./service-descriptions/params-by-name-petstore-openrpc.json')
 };
