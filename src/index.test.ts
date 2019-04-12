67e18503737392f94bd861dde509b5a7b3abf315
Zachary Belford <belfordz66@gmail.com>
2019-04-12 03:55:39 -0700

    fix: build is broken

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,13 +1,13 @@
-import metaSchema, { types } from "@open-rpc/meta-schema";
-import { parse } from "@open-rpc/schema-utils-js";
+import metaSchema, { OpenRPC } from "@open-rpc/meta-schema";
+import { parseOpenRPCDocument } from "@open-rpc/schema-utils-js";
 import openRpcExamples from "./index";
 import { forEach } from "lodash";
 
 describe("meta-schema validates all examples without error", () => {
   const exampleNames = Object.keys(openRpcExamples);
-  forEach(openRpcExamples, (example: types.OpenRPC, name: string) => {
+  forEach(openRpcExamples, (example: OpenRPC, name: string) => {
     it(`validates the example: ${name}`, async () => {
-      const result = await parse(JSON.stringify(example)) as types.OpenRPC;
+      const result = await parseOpenRPCDocument(JSON.stringify(example)) as OpenRPC;
       expect(typeof result).toBe("object");
       expect(result.methods.length).toBeGreaterThan(0);
     });
