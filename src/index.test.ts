a1d87d0e124799e60a32308fa91add4ba67c616d
fosgate29 <luizhamilton29@gmail.com>
2019-05-08 16:13:32 -0300

    refactor: removed unused variables

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,10 +1,9 @@
-import metaSchema, { OpenRPC } from "@open-rpc/meta-schema";
+import { OpenRPC } from "@open-rpc/meta-schema";
 import { parseOpenRPCDocument } from "@open-rpc/schema-utils-js";
 import openRpcExamples from "./index";
 import { forEach } from "lodash";
 
 describe("meta-schema validates all examples without error", () => {
-  const exampleNames = Object.keys(openRpcExamples);
   forEach(openRpcExamples, (example: OpenRPC, name: string) => {
     it(`validates the example: ${name}`, async () => {
       const result = await parseOpenRPCDocument(JSON.stringify(example)) as OpenRPC;
