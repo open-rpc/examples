d250f8d7a4b38b6eb3f13d4ef69d388391512c68
Zachary Belford <belfordz66@gmail.com>
2020-03-27 15:43:46 -0700

    fix: remove schema-utils cyc dep

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,6 +1,8 @@
-import { parseOpenRPCDocument } from "@open-rpc/schema-utils-js";
 import openRpcExamples from "./index";
 import { forEach, uniqBy, values } from "lodash";
+
+import refParser from "json-schema-ref-parser";
+import { OpenrpcDocument } from "@open-rpc/meta-schema";
 
 describe("meta-schema validates all examples without error", () => {
   it("has unique titles for each example", () => {
@@ -11,7 +13,7 @@ describe("meta-schema validates all examples without error", () => {
 
   forEach(openRpcExamples, (example, name: string) => {
     it(`validates the example: ${name}`, async () => {
-      const result = await parseOpenRPCDocument(example);
+      const result = await refParser.dereference(example) as OpenrpcDocument;
       expect(typeof result).toBe("object");
       expect(result.methods.length).toBeGreaterThan(0);
     });
